f1cb98da164bdf0a84edbb07938b68f63d301230
Your Name <you@example.com>
2025-04-28 23:41:12 +0300

    11th Edition - Fixed  A Bit2

--- college_books_project/settings.py
+++ college_books_project/settings.py
@@ -147,6 +147,9 @@ REST_FRAMEWORK = {
 
 STORAGES = {
     # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
