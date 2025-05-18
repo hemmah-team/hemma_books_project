bd2814109ebd14eb6f08c6629ea49133f9718e71
MSA <=>
2025-05-18 20:24:12 +0300

    fixed models

--- account/models.py
+++ account/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class UserManager(BaseUserManager):
     def create_user(
         self,
+        email,
         name,
         password,
         phone_number,
@@ -14,6 +15,8 @@ class UserManager(BaseUserManager):
         is_staff=False,
         is_superuser=False,
     ) -> "User":
+        if not email:
+            raise ValueError("The Email Can't Be Empty.")
         if not name:
             raise ValueError("The Name Can't Be Empty.")
 
@@ -23,7 +26,7 @@ class UserManager(BaseUserManager):
         if not password:
             raise ValueError("The Password Can't Be Empty.")
 
-        user = self.model(phone_number=phone_number)
+        user = self.model(email=email)
         user.name = name
         user.phone_number = phone_number
         user.fcm = fcm
@@ -38,6 +41,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(
         self,
+        email,
         name,
         password,
         phone_number,
@@ -46,6 +50,7 @@ class UserManager(BaseUserManager):
         is_banned,
     ) -> "User":
         user = self.create_user(
+            email=email,
             fcm=fcm,
             is_verified=is_verified,
             is_banned=is_banned,
@@ -62,20 +67,23 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
+    email = models.EmailField(
+        unique=True,
+    )
     name = models.CharField(max_length=50)
     password = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=10, unique=True)
     fcm = models.CharField(max_length=200)
     is_verified = models.BooleanField(default=False)
     is_banned = models.BooleanField(default=False)
-    email = None
     username = None
     first_name = None
     last_name = None
     objects = UserManager()
 
-    USERNAME_FIELD = "phone_number"
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = [
+        "phone_number",
         "name",
         "password",
         "fcm",
@@ -84,7 +92,7 @@ class User(AbstractUser):
     ]
 
     def __str__(self):
-        return self.name
+        return self.name + self.email
 
 
 class Otp(models.Model):
